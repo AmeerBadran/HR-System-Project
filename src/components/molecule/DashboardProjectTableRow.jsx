af1c2c7d1a28c2dad976789e1b819c925e7dc06c
ameer <abadran281@gmail.com>
2024-07-31 17:10:21 +0300

    creat some components in the dashboard :cards,chart,table3

--- src/components/molecule/DashboardProjectTableRow.jsx
+++ src/components/molecule/DashboardProjectTableRow.jsx
@@ -6,13 +6,13 @@ const DashboardProjectTableRow = ({ name, hours, priority, progress }) => {
       <td className="first-column border-b border-gray-600 p-3">{name}</td>
       <td className="border-b border-gray-600 p-3">{hours}</td>
       <td className="border-b border-gray-600 p-3">
-        <span className={`inline-block border ${priority === 'Low' ? 'text-[#08D25B] border-[#08D25B]':priority==='Medium' ? 'text-[#FFAB13] border-[#FFAB13]': priority==='High' ? 'text-[#EE414A] border-[#EE414A]':''} rounded-[4px] px-3 py-1`}>
+        <span className={`inline-block border ${priority === 'Low' ? 'text-[#08D25B] border-[#08D25B]' : priority === 'Medium' ? 'text-[#FFAB13] border-[#FFAB13]' : priority === 'High' ? 'text-[#EE414A] border-[#EE414A]' : ''} rounded-[4px] px-3 py-1`}>
           {priority}
         </span>
       </td>
       <td className="border-b border-gray-600 p-3">
-        <div className="progress-bar bg-white rounded-full overflow-hidden h-3">
-          <div className={`progress w-[${progress}%] h-full bg-purple-800`}></div>
+        <div className=" bg-[#2F323A] overflow-hidden h-3" title={progress}>
+          <div className={`h-full bg-purple-800`} style={{ width: progress }}></div>
         </div>
       </td>
     </tr>
@@ -23,7 +23,6 @@ DashboardProjectTableRow.propTypes = {
   name: PropTypes.string.isRequired,
   hours: PropTypes.number.isRequired,
   priority: PropTypes.string.isRequired,
-  priorityColor: PropTypes.string.isRequired,
   progress: PropTypes.string.isRequired,
 };
 
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -22,7 +22,7 @@ function Dashboard() {
         ))}
       </div>
       <div className="w-full grid 2md:grid-cols-3 grid-cols-1 gap-x-0 2md:gap-x-8 gap-y-8 2md:gap-y-0">
-        <div className="bg-[#191c24] p-6 rounded-[4px] w-full">
+        <div className="w-full bg-[#191c24] p-6 rounded-[4px] ">
           <h1 className="text-xl text-white font-semibold">Transaction History</h1>
           <DashboardChart />
           <TransactionItem
@@ -36,7 +36,7 @@ function Dashboard() {
             amount="$236"
           />
         </div>
-        <div className="bg-[#191c24] px-6 pt-4 rounded-[4px] col-span-2 w-full h-[520px]">
+        <div className="w-full bg-[#191c24] px-6 pt-4 rounded-[4px] col-span-2  h-[520px] overflow-y-auto scroll-bar scroll-p-0">
           <div className="text-white p-4 text-xl font-medium mb-5 border-b-2 border-gray-400">
             <h5>Active Projects</h5>
           </div>
