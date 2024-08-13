a1ded1221f31eb8714bf964a5a059a68aa084731
bayanlulu <bayanazzam4@gmail.com>
2024-08-13 13:08:07 +0300

    Update Contracts component to display contract data using DashboardProjectsTable

--- src/pages/Contracts.jsx
+++ src/pages/Contracts.jsx
@@ -1,11 +1,16 @@
 import PagesTitle from "../components/molecule/PagesTitle";
+import DashboardProjectsTable from "../components/organism/DashboardProjectsTable";
+import { useSelector } from 'react-redux';
+
 function Contracts() {
+  const contractsData = useSelector((state) => state.contracts);
+
   return (
-    <div className="w-full ">
-      <PagesTitle pageTitle={'Contracts'}/>
+    <div className="w-full">
+      <PagesTitle pageTitle="Contracts" />
+      <DashboardProjectsTable tableType="contracts" contractsData={contractsData} />
     </div>
-  )
+  );
 }
 
 export default Contracts;
-
