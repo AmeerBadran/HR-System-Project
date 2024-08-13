397768e78ab329b660c8154db38adfb36a7caafa
bayanlulu <bayanazzam4@gmail.com>
2024-08-13 12:56:09 +0300

    Set up Redux store with departments and contracts reducers

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import departmentReducer from '../features/departments/departmentsSlice';
+import contractsReducer from '../features/contracts/contractsSlice';
 
 const store = configureStore({
     reducer: {
         departments: departmentReducer,
+        contracts: contractsReducer,
     },
 });
 
